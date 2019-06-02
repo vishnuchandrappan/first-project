917cb5e2491b8ef72e218bf78c42cc0e931bc044
Vishnu Chandrappan <vishnuchandrappan@gmail.com>
2019-06-02 12:50:22 +0530

    added templates

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller{
 
   public function index(){
     $posts = Post::get();
-    return $posts;
+    return view('welcome',compact('posts'));
   }
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
 
+Route::get('/', 'PostController@index');
+
 Route::get('/create','PostController@create');
+
 Route::post('/posts','PostController@store');
 
-
-Route::get('/about', 'PostController@index');
-
-Route::get('/', function(){
-  return view('welcome');
-});
-
+Route::get('/about', 'PostController@about');
 
 Route::get('/contact', function (){
   return view('pages.about');
